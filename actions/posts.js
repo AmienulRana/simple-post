8e4e3cd2de362ee5583a7cc03b5f66646c09b365
AmienulRana <amienulrana@gmail.com>
2023-02-17 01:43:02 +0700

    menambahkan function getDetailPostsData untuk mengambil detail post, yang akan di generate oleh getStaticPaths'

--- actions/posts.js
+++ actions/posts.js
@@ -3,3 +3,8 @@ export async function getAllPostsData() {
   const posts = await res.json();
   return posts;
 }
+export async function getDetailPostsData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  const posts = await res.json();
+  return posts;
+}
